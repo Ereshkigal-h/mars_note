8ce3d7d25a5e44a369e4c34101e43e7456c7208d
ereshkigal <1292384382@qq.com>
2025-06-16 14:31:18 +0800

    更新notemapperxml文件

--- src/main/java/com/mars/mapper/NoteMapper.java
+++ src/main/java/com/mars/mapper/NoteMapper.java
@@ -11,13 +11,18 @@ import java.util.List;
 
 @Mapper
 public interface NoteMapper {
-//  输入用户id返回一个NoteId的字符串，不同id使用逗号分割
+//  输入用户id返回一个NoteId的字符串，不同id使用逗号分割,会直接返回到note对象里面
     void selectNoteId(Note note);
-    void selectNoteAddress(Note note);
+//  输入用户id返回一个NoteId的字符串，不同id使用逗号分割,返回一个字符串地址
+    String selectNoteAddress(String NoteId);
+//  第一次创建NoteId 会自动调用note对象
     void insertNoteId(Note note);
+//  更新NoteId，会自动调用note对象
     void updateNoteId(Note note);
-    void insertNoteAds(Note note);
-    void updateNoteAds(Note note);
+//  需要输入NoteId和noteAddress,适用于第一次创建
+    void insertNoteAds(String NoteId,String noteAddress);
+//  需要输入NoteId和noteAddress,适用于更新已有的值
+    void updateNoteAds(String NoteId,String noteAddress);
 
 }
 
