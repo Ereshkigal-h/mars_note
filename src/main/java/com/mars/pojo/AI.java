095a3081cc8b3e1a3d82e82e983bf0180aa9e12a
ereshkigal <1292384382@qq.com>
2025-06-17 22:31:54 +0800

    更新ai和aiserviceimpal

--- src/main/java/com/mars/pojo/AI.java
+++ src/main/java/com/mars/pojo/AI.java
@@ -8,4 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AI {
-    private String aiName;}
+    private String ApiName="spark x1";
+    private String Url="https://spark-api-open.xf-yun.com/v2/chat/completions";
+    private String ApiKey="ff2a334433420fc061bb6cda194cd9dc";
+    private String APISecret="ff2a334433420fc061bb6cda194cd9dc";
+}
---
+++ src/main/java/com/mars/service/impl/AiServicelmpl.java
@@ -0,0 +1,80 @@
+package com.mars.service.impl;
+
+import com.mars.pojo.User;
+import com.mars.pojo.AI;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AiServicelmpl {
+    String GetAiMessage(User user,String message,AI ai,String temperature) throws Exception {
+        long userId = user.userId;
+        String url = ai.getUrl();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.set("user", userId);
+        jsonObject.set("model", "x1");
+        JSONArray messagesArray = new JSONArray();
+        // 创建单个消息的JSON对象
+        JSONObject messageObject = new JSONObject();
+
+        messageObject.set("role", user.getUserName());
+        messageObject.set("content", message);
+        messageObject.set("temperature", temperature);
+
+        messagesArray.put(messageObject);
+        jsonObject.set("messages", messagesArray);
+        jsonObject.set("stream", true);
+        jsonObject.set("max_tokens", 4096);
+        String header = "Authorization: Bearer" + ai.getAPISecret();
+        URL obj;
+        try {
+            obj = new URL(url);
+        } catch (MalformedURLException e) {
+            throw new MalformedURLException("url不合法");
+        }
+        HttpURLConnection con;
+        try {
+            con = (HttpURLConnection) obj.openConnection();
+        } catch (IOException e) {
+            throw new Exception("连接错误");
+        }
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization", header);
+        con.setDoOutput(true);
+
+        OutputStream os = con.getOutputStream();
+        os.write(jsonObject.toString().getBytes());
+        os.flush();
+        os.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+            System.out.println(inputLine);
+        }
+        in.close();
+        String jsonStr = response.substring(0);
+        if (!jsonStr.isEmpty()) {
+            return jsonStr;
+
+        }
+        else {
+            return "";
+        }
+    }
+
+}
