909fc83a08555c8ac2870ea1393c14dc5b0ce897
ereshkigal <1292384382@qq.com>
2025-06-19 19:37:55 +0800

    aicontroller写好了

---
+++ src/main/java/com/mars/controller/AiController.java
@@ -0,0 +1,59 @@
+package com.mars.controller;
+
+import com.mars.pojo.AI;
+import com.mars.pojo.Result;
+import com.mars.pojo.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.mars.service.AiService;
+
+@Slf4j   //日志log
+@RequestMapping("/Ai")
+@RestController  // 自动返回json格式数据
+public class AiController {
+    @Autowired
+    private AiService aiService;
+
+    @PostMapping
+    public ResponseEntity<Object> AiMessage(@RequestBody User user, String message, AI ai, String temperature){
+        log.info("读取ai消息,user{}",user.getUserId());
+        try {
+            String ResultMassage = aiService.GetAiMessage(user, message, ai, temperature);
+            return new ResponseEntity<>(ResultMassage, HttpStatus.OK);
+        }
+        catch (Exception e){
+            log.error("ai读取失败");
+            return  new ResponseEntity<>("读取失败",HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
